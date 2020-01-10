20a70f66c3c43a4e79adf958a120498622237264
gegealf <gege.alf@gmail.com>
2020-01-10 10:25:55 +0100

    page d'accueil : ajout du logging

--- controleur.py
+++ controleur.py
@@ -1,5 +1,9 @@
 from flask import render_template
+import logging
+
+log = logging.getLogger(__name__)
 
 
-def hello():
-    return render_template("page_d_accueil.html")  # link to html page
+def page_d_accueil():
+    log.debug('connection starts')
+    return render_template("page_d_accueil.html")  # lien vers la page d'accueil
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask  # pip install flask
+from os import path
+import logging.config
+import logging
+
+# for use the ini/config file :
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log.config')
+logging.config.fileConfig(log_file_path)
+log = logging.getLogger(__name__)  # return a logger which current class is the root logger
 
 app = Flask(__name__)
 
@@ -9,7 +17,10 @@ if __name__ == "__main__":  # python main.py
 
     app.add_url_rule('/', 'pda', view_func=controleur.page_d_accueil)
     try:
+        log.info('Application starting')
         app.run(host='127.0.0.1', port=8000)  # method that run the application
         # app.run(host='0.0.0.0', port=4001)
+        log.info('Application end without exception')
     except Exception as ex:
+        log.exception('Application end because of uncatching exception')
         pass
