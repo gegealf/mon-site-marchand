024292f3b436973f88fecfd64307f3ed68970e06
gegealf <gege.alf@gmail.com>
2020-01-10 11:28:47 +0100

    page d'accueil : ajout des images pour des produits "cartes mémoires" avec grid/bootstrap/responsive

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from os import path
 import logging.config
 import logging
 
-# for use the ini/config file :
+# pour le fichier de config :
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log.config')
 logging.config.fileConfig(log_file_path)
-log = logging.getLogger(__name__)  # return a logger which current class is the root logger
+log = logging.getLogger(__name__)  # définition du logger pour la classe courante
 
 app = Flask(__name__)
 
 if __name__ == "__main__":  # python main.py
     import controleur
 
-    app.add_url_rule('/', 'pda', view_func=controleur.page_d_accueil)
+    app.add_url_rule('/', 'page_d_accueil', view_func=controleur.page_d_accueil)
     try:
         log.info('Application starting')
-        app.run(host='127.0.0.1', port=8000)  # method that run the application
+        app.run(host='127.0.0.1', port=8000)  # lancement de l'application en local
         # app.run(host='0.0.0.0', port=4001)
         log.info('Application end without exception')
     except Exception as ex:
