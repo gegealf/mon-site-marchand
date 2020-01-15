8a8efbe9c59a857e1d7caa239077101e9465a351
gegealf <47668438+gegealf@users.noreply.github.com>
2020-01-15 09:22:22 +0100

    Page d accueil (#1)
    * page d'accueil : ajout du logging
    
    * page d'accueil : ajout des images pour des produits "cartes mémoires" avec grid/bootstrap/responsive
    
    * page d'accueil : ajout haut de page et zone login
    
    * page d'accueil : ajout d'une search bar sous le titre
    
    * page d'accueil : + de responsive design
    
    * page d'accueil : ajout opage d'authentification html + css

--- controleur.py
+++ controleur.py
@@ -1,5 +1,14 @@
 from flask import render_template
+import logging
+
+log = logging.getLogger(__name__)
 
 
-def hello():
-    return render_template("page_d_accueil.html")  # link to html page
+def page_d_accueil():
+    log.debug('connexion à la page d\'accueil')
+    return render_template("page_d_accueil.html")  # lien vers la page d'accueil
+
+
+def authentification():
+    log.debug('connexion à la page d\'authentification')
+    return render_template("page_d_authentification.html")
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask  # pip install flask
+from os import path
+import logging.config
+import logging
+
+# pour le fichier de config :
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log.config')
+logging.config.fileConfig(log_file_path)
+log = logging.getLogger(__name__)  # définition du logger pour la classe courante
 
 app = Flask(__name__)
 
 if __name__ == "__main__":  # python main.py
     import controleur
 
-    app.add_url_rule('/', 'pda', view_func=controleur.page_d_accueil)
+    app.add_url_rule('/', 'page_d_accueil', view_func=controleur.page_d_accueil)
+    app.add_url_rule('/authentification', 'authentification', view_func=controleur.authentification)
+
     try:
-        app.run(host='127.0.0.1', port=8000)  # method that run the application
+        log.info('démarrage de l\'application')
+        app.run(host='127.0.0.1', port=8000)  # lancement de l'application en local
         # app.run(host='0.0.0.0', port=4001)
+        log.info('arrêt normal de l\'application')
     except Exception as ex:
+        log.exception('l\'application s\'est arretée à cause d\'une exception')
         pass
